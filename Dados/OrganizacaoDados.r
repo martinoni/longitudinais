e51ffd612a38ac918e8720e0cd7e73ded155b22e
bortoIME <48804370+bortoIME@users.noreply.github.com>
2019-08-26 15:10:13 -0300

    Dedo no cu e gritaria

--- Dados/OrganizacaoDados.r
+++ Dados/OrganizacaoDados.r
@@ -1,4 +1,5 @@
 library(readxl)
+library(ggplot2)
 
 DDVE <- read_excel("C:/Users/lucasb/Downloads/perfDDVEp_bait.xlsx")
 
@@ -18,3 +19,75 @@ for(i in 1:nrow(DDVE)){
   }
 }
 DDVE.l <- data.frame(id,grupo,semana,diam)
+
+
+ggplot(data=DDVE.l, aes(x=semana, y=diam, group = id,color=grupo)) +
+  geom_line()+
+  geom_point()
+
+diam <- c()
+grupo <- c()
+semana <- c()
+id <- c()
+for(j in 2:ncol(DDVE)){
+  id <- c(id,"mediaAIG")
+  grupo <- c(grupo,"AIG")
+  semana <- c(semana,(j+24))
+  media <- 0
+  n <- 0
+  for(i in 1:29){
+    if(!is.na(DDVE[i,j])){
+      media <- media + as.numeric(DDVE[i,j])
+      n <- n + 1
+    }
+  }
+  diam <- c(diam,media/n)
+  id <- c(id,"mediaPIG")
+  grupo <- c(grupo,"PIG")
+  semana <- c(semana,(j+24))
+  media <- 0
+  n <- 0
+  for(i in 30:nrow(DDVE)){
+    if(!is.na(DDVE[i,j])){
+      media <- media + as.numeric(DDVE[i,j])
+      n <- n + 1
+    }
+  }
+  diam <- c(diam,media/n)
+}
+
+media.df <- data.frame(id,grupo,semana,diam)
+tipo <- c(rep("indiv",284),rep("media",30))
+
+DDVE.t <- rbind(DDVE.l,media)
+DDVE.t <- cbind(DDVE.t,tipo)
+
+ggplot(data=DDVE.t, aes(x=semana, y=diam, group = id,color=grupo)) +
+  geom_line(aes(linetype = tipo, size = tipo))+
+  geom_point() +
+  scale_linetype_manual(values = c("solid", "dashed")) +
+  scale_color_manual(values = c("blue","red")) +
+  scale_size_manual(values = c(0.5,2))
+  
+
+aig <- subset(DDVE.t, grupo == "AIG")
+pig <- subset(DDVE.t, grupo == "PIG")
+
+ggplot(data=aig, aes(x=semana, y=diam, group = id)) +
+  geom_line(aes(linetype = tipo, size = tipo),colour="blue")+
+  geom_point(colour="blue") +
+  scale_linetype_manual(values = c("solid", "dashed")) +
+  scale_size_manual(values = c(0.5,2))
+
+
+ggplot(data=pig, aes(x=semana, y=diam, group = id)) +
+  geom_line(aes(linetype = tipo, size = tipo),colour="red")+
+  geom_point(colour="red") +
+  scale_linetype_manual(values = c("solid", "dashed")) +
+  scale_size_manual(values = c(0.5,2))
+
+ggplot(data=media.df, aes(x=semana, y=diam, group = id,color=grupo))+
+  geom_line() +
+  geom_point() +
+  scale_color_manual(values = c("blue","red"))
+
