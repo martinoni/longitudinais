f09cc1b34ee256531f1f4809c22341ad5a863924
martinoni <thiago.martinoni@usp.br>
2019-09-23 14:45:04 -0300

    coloca no github poha

---
+++ long_log.r
@@ -0,0 +1,191 @@
+
+library(readxl)
+library(ggplot2)
+
+setwd("C:/Users/Danvah/OneDrive/diagnostico_singer/DDVE")
+DDVE <- read_excel("perfDDVEp_bait.xlsx")
+for(i in 1:nrow(DDVE)){
+  for(j in 2:(ncol(DDVE))){
+    if(!is.na(DDVE[i,j])){
+      DDVE[i,j] <- log(DDVE[i,j])
+    }
+  }
+}
+
+
+id <- c()
+grupo <- c()
+diam <- c()
+semana <- c()
+
+for(i in 1:nrow(DDVE)){
+  for(j in 2:(ncol(DDVE))){
+    if(!is.na(DDVE[i,j])){
+      id <- c(id,i)
+      grupo <- c(grupo,DDVE$Grupo[i])
+      diam <- c(diam,as.numeric(DDVE[i,j]))
+      semana <- c(semana,(j+24))
+    }
+  }
+}
+DDVE.l <- data.frame(id,grupo,semana,diam)
+
+
+ggplot(data=DDVE.l, aes(x=semana, y=diam, group = id,color=grupo)) +
+  geom_line()+
+  geom_point()+
+  scale_color_manual(values = c("blue","red")) 
+
+diam <- c()
+grupo <- c()
+semana <- c()
+id <- c()
+for(j in 2:ncol(DDVE)){
+  id <- c(id,"mediaAIG")
+  grupo <- c(grupo,"AIG")
+  semana <- c(semana,(j+24))
+  media <- 0
+  n <- 0
+  for(i in 1:29){
+    if(!is.na(DDVE[i,j])){
+      media <- media + as.numeric(DDVE[i,j])
+      n <- n + 1
+    }
+  }
+  diam <- c(diam,media/n)
+  id <- c(id,"mediaPIG")
+  grupo <- c(grupo,"PIG")
+  semana <- c(semana,(j+24))
+  media <- 0
+  n <- 0
+  for(i in 30:nrow(DDVE)){
+    if(!is.na(DDVE[i,j])){
+      media <- media + as.numeric(DDVE[i,j])
+      n <- n + 1
+    }
+  }
+  diam <- c(diam,media/n)
+}
+
+media.df <- data.frame(id,grupo,semana,diam)
+tipo <- c(rep("indiv",284),rep("media",30))
+
+DDVE.t <- rbind(DDVE.l,media.df)
+DDVE.t <- cbind(DDVE.t,tipo)
+
+ggplot(data=DDVE.t, aes(x=semana, y=diam, group = id,color=grupo)) +
+  geom_line(aes(linetype = tipo, size = tipo))+
+  geom_point() +
+  scale_linetype_manual(values = c("solid", "dashed")) +
+  scale_color_manual(values = c("blue","red")) +
+  scale_size_manual(values = c(0.5,2))
+
+
+aig <- subset(DDVE.t, grupo == "AIG")
+pig <- subset(DDVE.t, grupo == "PIG")
+
+ggplot(data=aig, aes(x=semana, y=diam, group = id)) +
+  geom_line(aes(linetype = tipo, size = tipo),colour="blue")+
+  geom_point(colour="blue") +
+  scale_linetype_manual(values = c("solid", "dashed")) +
+  scale_size_manual(values = c(0.5,2))
+
+
+ggplot(data=pig, aes(x=semana, y=diam, group = id)) +
+  geom_line(aes(linetype = tipo, size = tipo),colour="red")+
+  geom_point(colour="red") +
+  scale_linetype_manual(values = c("solid", "dashed")) +
+  scale_size_manual(values = c(0.5,2))
+
+ggplot(data=media.df, aes(x=semana, y=diam, group = id,color=grupo))+
+  geom_line() +
+  geom_point() +
+  scale_color_manual(values = c("blue","red"))
+
+
+library(magrittr)
+library(dplyr)
+aux_AIG <- DDVE.l %>% filter(grupo == "AIG") %>% count(by = semana)
+aux_PIG <- DDVE.l %>% filter(grupo == "PIG") %>% count(by = semana)
+tabela <- inner_join(aux_AIG, aux_PIG, by = "by")
+names(tabela) <- c("Semana", "Quantidade de AIG", "Quantidade de PIG")
+tabela
+
+
+
+
+
+
+library(plyr)
+resumo = ddply(DDVE.t, .(grupo, semana), function(x){
+  c(mean=mean(x$diam), sd = sd(x$diam))
+})
+
+pd <- position_dodge(width = 0.3)
+ggplot(resumo, aes(x=semana, y=mean, color=grupo)) +
+  geom_line(aes(linetype=grupo)) +
+  geom_point(position=pd) +
+  geom_errorbar(aes(ymin=mean-sd, ymax=mean+sd), width=0.3, position=pd) +
+  scale_color_manual(values = c("blue","red"))
+
+
+
+
+
+
+ggplot(data=DDVE.t, aes(x=semana, y=diam, color=grupo)) +
+  geom_point() +
+  geom_smooth(aes(x=semana),method="loess",se = FALSE,span=1) +
+  scale_linetype_manual(values = c("solid", "dashed")) +
+  scale_color_manual(values = c("blue","red")) +
+  scale_size_manual(values = c(0.5,2))
+
+
+
+
+
+ggplot(data=aig, aes(x=semana, y=diam)) +
+  geom_point(colour="blue") +
+  stat_summary(fun.y=mean, geom="line",lwd=1.0) +
+  geom_smooth(aes(x=semana),method="loess",se = FALSE,span=1) +
+  scale_linetype_manual(values = c("solid", "dashed")) +
+  scale_size_manual(values = c(0.5,2))
+
+
+
+ggplot(data=pig, aes(x=semana, y=diam)) +
+  geom_point(colour="red") +
+  stat_summary(fun.y=mean, geom="line",lwd=1.0) +
+  geom_smooth(aes(x=semana),method="loess",se = FALSE,span=1) +
+  scale_linetype_manual(values = c("solid", "dashed")) +
+  scale_size_manual(values = c(0.5,2))
+
+
+
+
+
+ggplot(data=media.df, aes(x=semana, y=diam, group = id,color=grupo))+
+  geom_line() +
+  geom_point() +
+  scale_color_manual(values = c("blue","red"))
+
+
+
+
+
+ggplot(data=DDVE.l, aes(x=semana, y=diam, group = grupo,color=grupo)) +
+  stat_summary(fun.y=mean, geom="line",lwd=1.0) +
+  scale_color_manual(values = c("blue","red")) 
+
+
+
+
+
+ggplot(data=DDVE.l, aes(x=semana, y=diam, group = grupo,color=grupo)) +
+  geom_point()+
+  stat_summary(fun.y=mean, geom="line",lwd=1.0) +
+  scale_color_manual(values = c("blue","red")) 
+
+
+
+
---
+++ long_modelo.r
@@ -0,0 +1,64 @@
+
+
+library(readxl)
+library(ggplot2)
+
+setwd("C:/Users/Danvah/OneDrive/diagnostico_singer/DDVE")
+DDVE <- read_excel("perfDDVEp_bait.xlsx")
+
+id <- c()
+grupo <- c()
+diam <- c()
+semana <- c()
+
+for(i in 1:nrow(DDVE)){
+  for(j in 2:(ncol(DDVE))){
+    if(!is.na(DDVE[i,j])){
+      id <- c(id,i)
+      grupo <- c(grupo,DDVE$Grupo[i])
+      diam <- c(diam,as.numeric(DDVE[i,j]))
+      semana <- c(semana,(j+24))
+    }
+  }
+}
+DDVE.l <- data.frame(id,grupo,semana,diam)
+
+
+library(nlme)
+fit_1 <- lme(diam ~ I(as.numeric(grupo == 'AIG')) + 
+               I(as.numeric(grupo == 'PIG')) +
+               I(as.numeric(I(grupo == 'AIG'))*semana) +
+               I(as.numeric(I(grupo == 'PIG'))*semana) - 1 , random = ~ 1| id, data=DDVE.l)
+
+summary(fit_1)
+
+dados <- coef(fit_1)
+
+
+ggplot(data=DDVE.l, aes(x=semana, y=diam, group = grupo,color=grupo)) +
+  geom_abline(intercept = 25.533098, slope = -0.463335, color="blue") +
+  geom_abline(intercept = 30.347679 , slope = -0.559927, color="red") +
+  scale_x_continuous(limits = c(26, 40)) + 
+  scale_y_continuous(limits = c(6, 20)) +
+  geom_point()+
+  scale_color_manual(values = c("blue","red")) 
+
+
+
+DDVE.l1 <- subset(DDVE.l, id==1)
+ggplot(data=DDVE.l1, aes(x=semana, y=diam )) +
+  geom_abline(intercept = 25.533098+1.49045549, slope = -0.463335, color="blue") +
+  geom_point(color="blue")+
+  scale_x_continuous(limits = c(26, 40)) + 
+  scale_y_continuous(limits = c(6, 20)) +
+  scale_color_manual(values = c("blue","red")) 
+
+
+
+DDVE.l47 <- subset(DDVE.l, id==47)
+ggplot(data=DDVE.l47, aes(x=semana, y=diam )) +
+  geom_abline(intercept = 30.347679-1.93362013, slope = -0.559927, color="red") +
+  geom_point(color="red")+
+  scale_x_continuous(limits = c(26, 40)) + 
+  scale_y_continuous(limits = c(6, 20)) +
+  scale_color_manual(values = c("blue","red")) 
